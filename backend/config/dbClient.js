33638471f5ef8b518ea6a020753ae9466ce55e78
VictorKolev <viki.kolev01@gmail.com>
2025-05-11 13:08:15 +0200

    Logs Sequelize

--- backend/config/dbClient.js
+++ backend/config/dbClient.js
@@ -5,7 +5,8 @@ const sequelize = new Sequelize(
     process.env.MYSQL_DB, process.env.MYSQL_USER, process.env.MYSQL_PASSWORD,
     {
         host: process.env.MYSQL_HOST,
-        dialect: 'mysql'
+        dialect: 'mysql',
+        logging: process.env.NODE_ENV === 'development' ? console.log : false
     }
 )
 
